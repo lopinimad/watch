7e75a23baa33ff9e46297094ac743c377bd2e306
Your Name <your.email@example.com>
2025-07-17 20:32:06 +0100

    S

---
+++ src/pages/admin/AdminPage.tsx
@@ -0,0 +1,26 @@
+import { ThinContainer } from "@/components/layout/ThinContainer";
+import { Heading1, Paragraph } from "@/components/utils/Text";
+import { SubPageLayout } from "@/pages/layouts/SubPageLayout";
+import { ConfigValuesPart } from "@/pages/parts/admin/ConfigValuesPart";
+import { RegionSelectorPart } from "@/pages/parts/admin/RegionSelectorPart";
+import { TMDBTestPart } from "@/pages/parts/admin/TMDBTestPart";
+import { WorkerTestPart } from "@/pages/parts/admin/WorkerTestPart";
+
+import { BackendTestPart } from "../parts/admin/BackendTestPart";
+
+export function AdminPage() {
+  return (
+    <SubPageLayout>
+      <ThinContainer>
+        <Heading1>Admin tools</Heading1>
+        <Paragraph>Silly tools used test P-Stream! ૮₍´˶• . • ⑅ ₎ა</Paragraph>
+
+        <ConfigValuesPart />
+        <BackendTestPart />
+        <WorkerTestPart />
+        <TMDBTestPart />
+        <RegionSelectorPart />
+      </ThinContainer>
+    </SubPageLayout>
+  );
+}
