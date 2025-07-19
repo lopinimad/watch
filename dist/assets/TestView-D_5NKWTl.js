103ffad866c73be7b225ab308852a03a00a7a543
Lopin Issam <lopinimad@gmail.com>
2025-07-19 13:25:12 +0100

    Add files via upload

---
+++ dist/assets/TestView-D_5NKWTl.js
@@ -0,0 +1,2 @@
+import{r as e,j as s}from"./vendor-BWxiYVu1.js";import{B as o}from"./index-Bhg6ALQw.js";function i(){const[r,t]=e.useState(!1);if(r)throw new Error("I crashed");return s.jsx(o,{onClick:()=>t(!0),children:"Crash me!"})}export{i as default};
+//# sourceMappingURL=TestView-D_5NKWTl.js.map
